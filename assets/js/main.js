a5c8fcf349cb02e008ab6667c2508acbd2d5ffc8
Pierpaolo Calanna <p.calanna@gmail.com>
2023-12-24 19:19:16 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 import Alpine from 'alpinejs'
-import morph from '@alpinejs/morph'
 import persist from '@alpinejs/persist'
+import Htmx from 'htmx.org'
+window.Htmx = Htmx
 window.Alpine = Alpine
 Alpine.plugin(persist)
-Alpine.plugin(morph) 
 Alpine.store('lemmario', { "orderBy": Alpine.$persist("time") });
 Alpine.start()
---
+++ content/lemmi/m/minervale.md
@@ -0,0 +1,5 @@
+---
+title: minervale
+letter: M
+---
+minervale s. m. [dal lat. minerval; cfr. la voce prec.]. – Nell’antica Roma, onorario degli insegnanti, così detto perché veniva consegnato il 19 marzo, festa di Minerva.
